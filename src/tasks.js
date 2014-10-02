603c31e8cdb904e44e5224400d8607e5f02bdc35
Cam Wheeler <cameronwheeler@eftsource.com>
2014-10-02 16:14:44 -0500

    page is loading data from json file

---
+++ src/tasks.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function(){
+   
+   loaddata();
+
+   $('.taskStatus').on('click', 'button', function(){
+      var sdf = $(this).closest('button');
+      if (sdf[0].innerText == 'Start'){
+         sdf[0].innerText = 'Finish';
+      }
+      else if (sdf[0].innerText == 'Finish'){
+         sdf[0].innerText = 'Done';
+         sdf[0].disabled = true;
+      }
+   });
+});
+
+function loaddata(){
+var kid = 'Cassidy';
+$.getJSON('tasks.json',
+   function(data){
+      var tasks = $.grep(data.kids, function(e){
+         return e.Name==kid
+      })[0].Tasks;
+      
+      var row = '';
+      for (var i in tasks){
+         var task = tasks[i];
+         row+='<tr><td>'+task.Name+'</td>';
+         row+='<td>'+task.Description+'</td>';
+         row+='<td><button>Start</button></td></tr>';
+      }
+      $(row).appendTo('.taskStatus');
+   });
+};
