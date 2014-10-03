60400862f304f89c1fabe251b967eb3531cd5538
Cam Wheeler <cameronwheeler@eftsource.com>
2014-10-03 11:01:28 -0500

    improved look

--- src/tasks.js
+++ src/tasks.js
@@ -1,4 +1,6 @@
-﻿$(document).ready(function(){
+﻿var asData;
+
+$(document).ready(function(){
    
    loaddata();
 
@@ -6,10 +8,14 @@
       var sdf = $(this).closest('button');
       if (sdf[0].innerText == 'Start'){
          sdf[0].innerText = 'Finish';
+         $(sdf).removeClass('buttonStart');
+         $(sdf).addClass('buttonFinish');
       }
       else if (sdf[0].innerText == 'Finish'){
          sdf[0].innerText = 'Done';
          sdf[0].disabled = true;
+         $(sdf).removeClass('buttonFinish');
+         $(sdf).addClass('buttonDone');
       }
    });
 });
@@ -18,6 +24,7 @@ function loaddata(){
 var kid = 'Cassidy';
 $.getJSON('tasks.json',
    function(data){
+      asData = data;
       var tasks = $.grep(data.kids, function(e){
          return e.Name==kid
       })[0].Tasks;
@@ -27,7 +34,7 @@ $.getJSON('tasks.json',
          var task = tasks[i];
          row+='<tr><td>'+task.Name+'</td>';
          row+='<td>'+task.Description+'</td>';
-         row+='<td><button>Start</button></td></tr>';
+         row+='<td><button class="buttonStart">Start</button></td></tr>';
       }
       $(row).appendTo('.taskStatus');
    });
